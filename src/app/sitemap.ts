49443385eb76d474d09497b2a0b4ea2f281f901e
mjkid221 <jamerytstudio@gmail.com>
2026-01-26 13:44:40 +1000

    chore: fix url domain

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.fff-bridge.com",
+      url: "https://www.fff-bridge.net",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
